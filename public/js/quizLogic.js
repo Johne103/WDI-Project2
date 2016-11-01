5a2fd4a3d3d204473da5ce8eaab0f8069fd558d0
John Evans <johne103@hotmail.com>
2016-11-01 11:40:08 +0000

    Create question function

--- public/js/quizLogic.js
+++ public/js/quizLogic.js
@@ -7,6 +7,7 @@ $(function () {
   var currentCapital = "";
   var isCountry = "";
   var answerToQuestion = "";
+  var numberOfQuestionOptions = 0; //Number of options to select for each question.
 
   // create array of objects of all countries with properties name, capital, alpha2Code and latLng.
   $.get("https://restcountries.eu/rest/v1/all").done(function (data) {
@@ -15,6 +16,11 @@ $(function () {
         name: country.name,
         capital: country.capital,
         id: country.alpha2Code,
+        population: country.population,
+        region: country.region,
+        subRegion: country.subregion,
+        area: country.area,
+        borders: country.borders,
         location: {
           lat: country.latlng[0],
           lng: country.latlng[1]
@@ -40,6 +46,8 @@ $(function () {
     var randomIndex = Math.floor(Math.random() * countryData.length);
     return countryData[randomIndex];
   }
+
+  numberOfQuestionOptions = 3;
 
   //Select first four countries from randam array and check for duplicate selectioins.
   function selectCountries(alpha2Code) {
@@ -85,23 +93,29 @@ $(function () {
     // console.log(selectedCountries);
 
     var isCountry = currentCountry;
-    console.log(isCountry);
-    console.log(currentCountry);
+    console.log("isCountry: " + isCountry);
+    console.log("currentCountry: " + currentCountry);
     // document.getElementById('whichCountry').value = isCountry;
 
-    $("#quiz").html("\n      <p>Select the capital of: </p><label id=\"whichCountry\"></label>\n      <label>" + selectedCountries[0].capital + "</label>\n      <input type=\"radio\" name=\"answer\" value=\"" + selectedCountries[0].capital + "\">\n      <label>" + selectedCountries[1].capital + "</label>\n      <input type=\"radio\" name=\"answer\" value=\"" + selectedCountries[1].capital + "\">\n      <label>" + selectedCountries[2].capital + "</label>\n      <input type=\"radio\" name=\"answer\" value=\"" + selectedCountries[2].capital + "\">\n      <label>" + selectedCountries[3].capital + "</label>\n      <input type=\"radio\" name=\"answer\" value=\"" + selectedCountries[3].capital + "\">\n\n      ");
 
-    //Check for correct answer and return true or false.
-    $('input:radio[name="answer"]').change(function () {
-      if ($(this).val() == currentCapital) {
-        answerToQuestion = true;
-        console.log(answerToQuestion);
-        console.log('correct selected: ' + currentCapital);
-      } else {
-        answerToQuestion = false;
-        console.log(answerToQuestion);
-        console.log('correct not selected: ' + currentCapital);
-      }
-    });
+    var askQuestion = function askQuestion(option1, option2, option3, option4) {
+
+      $("#quizPopup").html("\n        <p>Select the capital of: </p><label id=\"whichCountry\"></label>\n\n        <label>" + option1 + "</label>\n        <input type=\"radio\" name=\"answer\" value=\"" + option1 + "\">\n        <label>" + option2 + "</label>\n        <input type=\"radio\" name=\"answer\" value=\"" + option2 + "\">\n        <label>" + option3 + "</label>\n        <input type=\"radio\" name=\"answer\" value=\"" + option3 + "\">\n        <label>" + option4 + "</label>\n        <input type=\"radio\" name=\"answer\" value=\"" + option4 + "\">\n\n        ");
+
+      //Check for correct answer and return true or false.
+      $('input:radio[name="answer"]').change(function () {
+        if ($(this).val() == currentCapital) {
+          answerToQuestion = true;
+          console.log("Answer: " + answerToQuestion);
+          console.log('correct selected: ' + currentCapital);
+        } else {
+          answerToQuestion = false;
+          console.log("Answer: " + answerToQuestion);
+          console.log('correct not selected: ' + currentCapital);
+        }
+      });
+    };
+    //First Question
+    askQuestion(selectedCountries[0].capital, selectedCountries[1].capital, selectedCountries[2].capital, selectedCountries[3].capital);
   }
 });
