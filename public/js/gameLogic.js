bc8b8b67c4b283c18d743ab9097960d89264a4ed
Jamie Simpson <jamie.shaun.simpson12@gmail.com>
2016-11-01 12:38:48 +0000

    removed unused gameLogic file.

--- public/js/gameLogic.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-var $main = $('main');
-
-$main.html('\n    <div class="playerPowerCounters">\n      <p id="playerOnePower">10</p>\n      <p id="playerTwoPower">10</p>\n    </div>\n    ');
